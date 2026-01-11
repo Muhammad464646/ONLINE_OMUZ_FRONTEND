a5a001b446c5cb80943a546c0751fcdab2e0d708
Mbuhoriev55@gmai.com <Mbuhoriev55@gmail.com>
2026-01-11 12:06:59 +0500

    CourseCard done

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'antd';
 
-const CourseCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const CourseCard = ({ course }) => {
+    return (
+        <Card
+            hoverable
+            cover={course.image && <img alt={course.title} src={course.image} />}
+        >
+            <Card.Meta title={course.title} description={course.description} />
+        </Card>
+    );
 }
 
-export default CourseCard
+export default CourseCard;
