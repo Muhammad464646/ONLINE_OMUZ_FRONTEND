2d2d3387f30488d877fb108efab8a7954dfe4672
Mbuhoriev55@gmai.com <Mbuhoriev55@gmail.com>
2026-01-11 12:14:54 +0500

    Routing+HomePage minimal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import API from '../services/api';
+import { Row, Col } from 'antd';
+import CourseCard from '../components/CourseCard';
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        API.get('courses/')
+            .then(res => setCourses(res.data))
+            .catch(err => console.log(err));
+    }, []);
+
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>Все курсы</h1>
+            <Row gutter={[16, 16]}>
+                {courses.map(course => (
+                    <Col key={course.id} span={6}>
+                        <CourseCard course={course} />
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
