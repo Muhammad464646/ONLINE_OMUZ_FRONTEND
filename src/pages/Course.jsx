64f78f1e3e25d63f9608a46233fc564337a1d78d
Mbuhoriev55@gmai.com <Mbuhoriev55@gmail.com>
2026-01-12 14:16:39 +0500

    finish courses Page

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,9 +1,11 @@
-import { Col, Row } from 'antd';
+import { Col, Row, Select } from 'antd';
 import React, { useEffect, useState } from 'react'
 import API from '../services/api';
 import CourseCard from '../components/CourseCard';
+import { Option } from 'antd/es/mentions';
 const Course = () => {
   const [courses, setCourses] = useState([]);
+  const [value,setValue]=useState('all')
 
   useEffect(() => {
     API.get('courses/')
@@ -11,11 +13,16 @@ const Course = () => {
       .catch(err => console.log(err));
   }, []);
   return (
-    <div className='bg-gray-50'>
-      <div style={{ padding: '20px' }}>
-        <h1 className='text-center text-3xl font-bold'> Чизи нав омӯзед!</h1>
+    <div className='bg-gray-100 pt-20 '>
+      <div style={{ padding: '20px' }} className='w-[90%] m-auto' >
+        <h1 className=' mb-10 text-3xl font-semibold'> Чизи нав омӯзед!</h1>
+        <Select className='w-50 ' value={value} onChange={(val)=>setValue(val)} placeholder='Выберите категорию'>
+          <Select.Option className='text-[20px] font-serif' value="all">Хама курсхо</Select.Option>
+          <Select.Option className='text-[20px] font-serif' value="Backend">Бекенд</Select.Option>
+          <Select.Option  className='text-[20px] font-serif' value="Frontend">Фронтенд</Select.Option>
+        </Select><br /><br /><br />
         <Row gutter={[16, 16]}>
-          {courses.map(course => (
+          {courses.filter(el=>value=='all'|| el.category==value).map(course => (
             <Col key={course.id} span={6}>
               <CourseCard course={course} />
             </Col>
